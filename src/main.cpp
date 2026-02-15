0f1837c8a976c399bee812fc0f9ced61e6524769
Elio OUEDRAOGO <elio.oued.work@gmail.com>
2026-02-15 18:25:01 +0100

    Rendered cube with triangles

--- src/main.cpp
+++ src/main.cpp
@@ -29,46 +29,82 @@ int main(int, char**)
   double L    = 2.0f;
   double half = L / 2.0f;
 
-  std::vector<linalg::Vector<double, 3>> front = {
-      { half,  half, half},
-      {-half,  half, half},
-      {-half, -half, half},
-      { half, -half, half},
+  const std::vector<linalg::Vector<double, 3>> cube_vertex = {
+      {-1.0f, -1.0f, -1.0f}, // triangle 1 : begin
+      {-1.0f, -1.0f,  1.0f},
+      {-1.0f,  1.0f,  1.0f}, // triangle 1 : end
+      { 1.0f,  1.0f, -1.0f}, // triangle 2 : begin
+      {-1.0f, -1.0f, -1.0f},
+      {-1.0f,  1.0f, -1.0f}, // triangle 2 : end
+      { 1.0f, -1.0f,  1.0f},
+      {-1.0f, -1.0f, -1.0f},
+      { 1.0f, -1.0f, -1.0f},
+      { 1.0f,  1.0f, -1.0f},
+      { 1.0f, -1.0f, -1.0f},
+      {-1.0f, -1.0f, -1.0f},
+      {-1.0f, -1.0f, -1.0f},
+      {-1.0f,  1.0f,  1.0f},
+      {-1.0f,  1.0f, -1.0f},
+      { 1.0f, -1.0f,  1.0f},
+      {-1.0f, -1.0f,  1.0f},
+      {-1.0f, -1.0f, -1.0f},
+      {-1.0f,  1.0f,  1.0f},
+      {-1.0f, -1.0f,  1.0f},
+      { 1.0f, -1.0f,  1.0f},
+      { 1.0f,  1.0f,  1.0f},
+      { 1.0f, -1.0f, -1.0f},
+      { 1.0f,  1.0f, -1.0f},
+      { 1.0f, -1.0f, -1.0f},
+      { 1.0f,  1.0f,  1.0f},
+      { 1.0f, -1.0f,  1.0f},
+      { 1.0f,  1.0f,  1.0f},
+      { 1.0f,  1.0f, -1.0f},
+      {-1.0f,  1.0f, -1.0f},
+      { 1.0f,  1.0f,  1.0f},
+      {-1.0f,  1.0f, -1.0f},
+      {-1.0f,  1.0f,  1.0f},
+      { 1.0f,  1.0f,  1.0f},
+      {-1.0f,  1.0f,  1.0f},
+      { 1.0f, -1.0f,  1.0f}
   };
 
-  std::vector<linalg::Vector<double, 3>> back = {
-      { half, -half, -half}, // bottom-right
-      {-half, -half, -half}, // bottom-left
-      {-half,  half, -half}, // top-left
-      { half,  half, -half}, // top-right
-  };
-
-  std::vector<linalg::Vector<double, 3>> left = {
-      {-half,  half,  half}, // top-front
-      {-half,  half, -half}, // top-back
-      {-half, -half, -half}, // bottom-back
-      {-half, -half,  half}, // bottom-front
-  };
-
-  std::vector<linalg::Vector<double, 3>> right = {
-      {half,  half, -half}, // top-back
-      {half,  half,  half}, // top-front
-      {half, -half,  half}, // bottom-front
-      {half, -half, -half}, // bottom-back
-  };
-
-  std::vector<linalg::Vector<double, 3>> top = {
-      { half, half, -half}, // top-right back
-      {-half, half, -half}, // top-left back
-      {-half, half,  half}, // top-left front
-      { half, half,  half}, // top-right front
-  };
-
-  std::vector<linalg::Vector<double, 3>> bottom = {
-      { half, -half,  half}, // bottom-right front
-      {-half, -half,  half}, // bottom-left front
-      {-half, -half, -half}, // bottom-left back
-      { half, -half, -half}, // bottom-right back
+  const std::vector<linalg::Vector<double, 3>> cube_colors = {
+      {0.583f, 0.771f, 0.014f},
+      {0.609f, 0.115f, 0.436f},
+      {0.327f, 0.483f, 0.844f},
+      {0.822f, 0.569f, 0.201f},
+      {0.435f, 0.602f, 0.223f},
+      {0.310f, 0.747f, 0.185f},
+      {0.597f, 0.770f, 0.761f},
+      {0.559f, 0.436f, 0.730f},
+      {0.359f, 0.583f, 0.152f},
+      {0.483f, 0.596f, 0.789f},
+      {0.559f, 0.861f, 0.639f},
+      {0.195f, 0.548f, 0.859f},
+      {0.014f, 0.184f, 0.576f},
+      {0.771f, 0.328f, 0.970f},
+      {0.406f, 0.615f, 0.116f},
+      {0.676f, 0.977f, 0.133f},
+      {0.971f, 0.572f, 0.833f},
+      {0.140f, 0.616f, 0.489f},
+      {0.997f, 0.513f, 0.064f},
+      {0.945f, 0.719f, 0.592f},
+      {0.543f, 0.021f, 0.978f},
+      {0.279f, 0.317f, 0.505f},
+      {0.167f, 0.620f, 0.077f},
+      {0.347f, 0.857f, 0.137f},
+      {0.055f, 0.953f, 0.042f},
+      {0.714f, 0.505f, 0.345f},
+      {0.783f, 0.290f, 0.734f},
+      {0.722f, 0.645f, 0.174f},
+      {0.302f, 0.455f, 0.848f},
+      {0.225f, 0.587f, 0.040f},
+      {0.517f, 0.713f, 0.338f},
+      {0.053f, 0.959f, 0.120f},
+      {0.393f, 0.621f, 0.362f},
+      {0.673f, 0.211f, 0.457f},
+      {0.820f, 0.883f, 0.371f},
+      {0.982f, 0.099f, 0.879f}
   };
 
   std::vector<linalg::Vector<double, 3>> x_axis = {
@@ -88,7 +124,6 @@ int main(int, char**)
   double origin2D_x = 100;
   double origin2D_y = 100;
 
-  auto faces3d = {front, back, left, right, top, bottom};
   std::vector<std::vector<linalg::Vector<double, 3>>> axes = {x_axis, y_axis,
                                                               z_axis};
   std::vector<std::vector<int>> colors                     = {
@@ -134,24 +169,26 @@ int main(int, char**)
 
       SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
       SDL_RenderClear(renderer);
-      SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF);
-
-      for (std::vector<linalg::Vector<double, 3>> face : faces3d)
+      SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xFF);
+      for (size_t i = 0; i < cube_vertex.size(); i += 3)
       {
-        for (long unsigned int i = 0; i < face.size(); i++)
+        SDL_Vertex vertex[3];
+        for (int j = 0; j < 3; j++)
         {
-          linalg::Vector<double, 3> l1 = face[i];
-          linalg::Vector<double, 3> l2 = face[(i + 1) % face.size()];
+          linalg::Vector<double, 3> l = cube_vertex[i + j];
+          l = linalg::Matrix<int, 3, 3>::ScalingMatrix({10, 10, 10}) * l;
+          l = arcball.rotate(l);
+          l += linalg::Vector<double, 3>{origin2D_x, origin2D_y, 100};
+          vertex[j].position.x = l[0];
+          vertex[j].position.y = l[1];
 
-          l1 = arcball.rotate(l1);
-          l2 = arcball.rotate(l2);
-
-          l1 = linalg::Matrix<int, 3, 3>::ScalingMatrix({10, 10, 10}) * l1;
-          l2 = linalg::Matrix<int, 3, 3>::ScalingMatrix({10, 10, 10}) * l2;
-
-          SDL_RenderLine(renderer, l1[0] + origin2D_x, l1[1] + origin2D_y,
-                         l2[0] + origin2D_x, l2[1] + origin2D_y);
+          vertex[j].color.r = cube_colors[i + j][0];
+          vertex[j].color.g = cube_colors[i + j][1];
+          vertex[j].color.b = cube_colors[i + j][2];
+          vertex[j].color.a = 1.0;
         }
+
+        SDL_RenderGeometry(renderer, NULL, vertex, 3, NULL, 0);
       }
       for (int i = 0; i < 3; i++)
       {
