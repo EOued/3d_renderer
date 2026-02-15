2ab26408530ad61abccebee5e53f981d4085c7d0
Elio OUEDRAOGO <elio.oued.work@gmail.com>
2026-02-15 18:01:37 +0100

    Added cube

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,51 @@ int main(int, char**)
   SDL_CreateWindowAndRenderer("Renderer", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE,
                               &window, &renderer);
 
+  double L    = 2.0f;
+  double half = L / 2.0f;
+
+  std::vector<linalg::Vector<double, 3>> front = {
+      { half,  half, half},
+      {-half,  half, half},
+      {-half, -half, half},
+      { half, -half, half},
+  };
+
+  std::vector<linalg::Vector<double, 3>> back = {
+      { half, -half, -half}, // bottom-right
+      {-half, -half, -half}, // bottom-left
+      {-half,  half, -half}, // top-left
+      { half,  half, -half}, // top-right
+  };
+
+  std::vector<linalg::Vector<double, 3>> left = {
+      {-half,  half,  half}, // top-front
+      {-half,  half, -half}, // top-back
+      {-half, -half, -half}, // bottom-back
+      {-half, -half,  half}, // bottom-front
+  };
+
+  std::vector<linalg::Vector<double, 3>> right = {
+      {half,  half, -half}, // top-back
+      {half,  half,  half}, // top-front
+      {half, -half,  half}, // bottom-front
+      {half, -half, -half}, // bottom-back
+  };
+
+  std::vector<linalg::Vector<double, 3>> top = {
+      { half, half, -half}, // top-right back
+      {-half, half, -half}, // top-left back
+      {-half, half,  half}, // top-left front
+      { half, half,  half}, // top-right front
+  };
+
+  std::vector<linalg::Vector<double, 3>> bottom = {
+      { half, -half,  half}, // bottom-right front
+      {-half, -half,  half}, // bottom-left front
+      {-half, -half, -half}, // bottom-left back
+      { half, -half, -half}, // bottom-right back
+  };
+
   std::vector<linalg::Vector<double, 3>> x_axis = {
       {0, 0, 0},
       {2, 0, 0}
@@ -40,7 +85,10 @@ int main(int, char**)
       {0, 0, 0},
       {0, 0, 2}
   };
+  double origin2D_x = 100;
+  double origin2D_y = 100;
 
+  auto faces3d = {front, back, left, right, top, bottom};
   std::vector<std::vector<linalg::Vector<double, 3>>> axes = {x_axis, y_axis,
                                                               z_axis};
   std::vector<std::vector<int>> colors                     = {
@@ -88,8 +136,23 @@ int main(int, char**)
       SDL_RenderClear(renderer);
       SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF);
 
-      // Convertion to canonical space
+      for (std::vector<linalg::Vector<double, 3>> face : faces3d)
+      {
+        for (long unsigned int i = 0; i < face.size(); i++)
+        {
+          linalg::Vector<double, 3> l1 = face[i];
+          linalg::Vector<double, 3> l2 = face[(i + 1) % face.size()];
 
+          l1 = arcball.rotate(l1);
+          l2 = arcball.rotate(l2);
+
+          l1 = linalg::Matrix<int, 3, 3>::ScalingMatrix({10, 10, 10}) * l1;
+          l2 = linalg::Matrix<int, 3, 3>::ScalingMatrix({10, 10, 10}) * l2;
+
+          SDL_RenderLine(renderer, l1[0] + origin2D_x, l1[1] + origin2D_y,
+                         l2[0] + origin2D_x, l2[1] + origin2D_y);
+        }
+      }
       for (int i = 0; i < 3; i++)
       {
         std::vector<linalg::Vector<double, 3>> axe = axes[i];
@@ -102,13 +165,6 @@ int main(int, char**)
 
         l1 = linalg::Matrix<int, 3, 3>::ScalingMatrix({10, 10, 10}) * l1;
         l2 = linalg::Matrix<int, 3, 3>::ScalingMatrix({10, 10, 10}) * l2;
-
-        // // To 2D :
-
-        double perspective_1 = 100.0f / (100.0f + l1[2]);
-        double perspective_2 = 100.0f / (100.0f + l2[2]);
-        l1 *= perspective_1;
-        l2 *= perspective_2;
 
         SDL_SetRenderDrawColor(renderer, color[0], color[1], color[2], 0xFF);
         SDL_RenderLine(renderer, l1[0] + 10, l1[1] + 10, l2[0] + 10,
