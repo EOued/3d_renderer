4b78c4f9833204e80d7f87fa71aa1ba5f914a5fd
Elio OUEDRAOGO <elio.oued.work@gmail.com>
2026-02-16 14:50:02 +0100

    Fixed issues

--- src/main.cpp
+++ src/main.cpp
@@ -70,8 +70,8 @@ int main(int argc, char* argv[])
   glBindTexture(GL_TEXTURE_2D, texture);
 
   // Set wrapping / filtering
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
